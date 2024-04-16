39710b8626e23cdcbe7b54b7020d8e3afb0b6e6e
Alessandra Versari <alessandra.versari2@studio.unibo.it>
2024-04-16 16:15:03 +0200

    feat: aggiunta possibilità di collegare due entità tramite un'associazione

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 var namespace = joint.shapes;
 var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+var diagramEntities = []; 
+var selectedRectangles = []; 
 
 /*counters*/
 var entityCounter = 0;
@@ -37,61 +39,57 @@ document.querySelector('.drawContainer').addEventListener('click', function(even
         rect.resize(150, 80);
         rect.attr({
             body: {
-                fill: 'white'
+                fill: 'white',
+                cursor: 'pointer'
             },
             label: {
-                text: 'ENTITY' /*aggiungere counter entity*/,
+                text: 'ENTITY' + entityCounter,
                 fill: 'black',
                 fontSize: 16
-
             }
         });
         rect.addTo(graph);
+        diagramEntities.push(rect);  //in un vettore mi salvo tutte le entità che aggiungo
         buttonEntitySelected = false;
     }
-    else if(buttonCreateRelationSelected === true){
-        var rel = new joint.shapes.standard.Polygon();
-        rel.attr('root/title', 'joint.shapes.standard.Polygon');
-        rel.attr('label/text', 'RELATION');
-        rel.attr('body/refPoints', '0,10 10,0 20,10 10,20');
-        rel.attr('body/fill', 'white');
-        rel.addTo(graph);
-        buttonCreateRelationSelected = false;
-    } 
-});
+    }
+);
 
+
+//Quando l'utente clicca sul bottone relation
 document.querySelector('.buttonCreateRelation').addEventListener('click', function(){
     relationCounter++;
     buttonCreateRelationSelected = true;
 })
 
+// Funzione per creare un'associazione tra due entità
+function createLinkBetweenEntities(entity1, entity2) {
+    var link = new joint.shapes.standard.Link({
+        source: entity1,
+        target: entity2
+    });
+    graph.addCell(link);
+}
 
-/*var rect = new joint.shapes.standard.Rectangle();
-rect.position(100, 30);
-rect.resize(100, 40);
-rect.attr({
-    body: {
-        fill: 'blue'
-    },
-    label: {
-        text: 'Hello',
-        fill: 'white'
+// Aggiungi l'evento di doppio clic a tutti i rettangoli
+paper.on('element:pointerdblclick', function(cellView) {
+    var cell = cellView.model;
+    if (cell.isElement() && cell.attributes.type === 'standard.Rectangle' && buttonCreateRelationSelected) {
+        if (!selectedRectangles.includes(cell)) {
+            selectedRectangles.push(cell);
+            console.log('Rettangolo selezionato');
+            cell.attr('body/stroke', 'yellow');
+        }
+        if (selectedRectangles.length === 2) {
+            createLinkBetweenEntities(selectedRectangles[0], selectedRectangles[1]);
+            selectedRectangles[0].attr('body/stroke', 'purple');
+            selectedRectangles[1].attr('body/stroke', 'purple');
+            selectedRectangles = []; //svuoto il vettore delle entità selezionate
+            buttonCreateRelationSelected = false; //deselezione il pulsante per le relations
+        }
     }
 });
 
-rect.addTo(graph);
-
-var rect2 = rect.clone();
-rect2.translate(300, 0);
-rect2.attr('label/text', 'World!');
-rect2.addTo(graph);
-
-var link = new joint.shapes.standard.Link();
-link.source(rect);
-link.target(rect2);
-link.addTo(graph);*/
-
-//paper.translate(300, 50); permette di spostare il diagramma disegnato
 })
 
 
